f456e3a8e7ebf44e8a15fce54782d5a3d622ffb7
Mathias SUCHAIL <mathias.suchail@yahoo.com>
2023-06-07 12:53:38 +0200

    TP Puissance 4 : prise en charge des grilles customisées

--- JS/Puissance 4/index.js
+++ JS/Puissance 4/index.js
@@ -1,23 +1,36 @@
-const nbCol = 7;
-const nbRow = 6;
+let nbCol = 7;
+let nbRow = 6;
+let pow = 5;
 let tour = 1;
 
 const row = [];
 const grid = [];
 
-const gridTarget = document.querySelector("#grid");
+function settings() {
+  document.querySelector("#settings form").onsubmit = function (event) {
+    event.preventDefault();
+    document.querySelector("#settings").style = "display: none;";
+    nbRow = event.target[0].value;
+    nbCol = event.target[1].value;
+    pow = event.target[2].value;
+    generateHTML(nbCol, nbRow);
+    cellsEvent();
+  };
+}
 
-function generateHTMLGrid(nbCol, nbRow) {
+function generateHTML(nbCol, nbRow) {
+  document.querySelector("h1").innerText = "Puissance " + pow;
+  document.querySelector("h2").innerText = "Au tour du joueur " + tour;
   for (let j = nbRow - 1; j >= 0; j--) {
     const HTMLRow = document.createElement("div");
     HTMLRow.classList.add("row");
     for (let i = 0; i < nbCol; i++) {
       const HTMLCell = document.createElement("div");
       HTMLCell.classList.add("cell");
-      HTMLCell.setAttribute("id", `${j}${i}`);
+      HTMLCell.setAttribute("id", `${j}.${i}`);
       HTMLRow.appendChild(HTMLCell);
     }
-    gridTarget.appendChild(HTMLRow);
+    document.querySelector("#grid").appendChild(HTMLRow);
   }
 }
 
@@ -38,14 +51,15 @@ function cellsEvent() {
 
 function gravity(event) {
   let newID = "";
-  let player = event.target.classList.contains("J1");
-  player ? (player = "J1") : (player = "J2");
+  let player;
+  tour === 1 ? (player = "J2") : (player = "J1");
+
   for (let i = event.target.id[0]; i > 0; i--) {
     const bottomCell = {
-      id: `${i - 1}${event.target.id[1]}`,
+      id: `${i - 1}.${event.target.id.split(".")[1]}`,
       empty:
-        document.getElementById(`${i - 1}${event.target.id[1]}`).classList
-          .length < 2,
+        document.getElementById(`${i - 1}.${event.target.id.split(".")[1]}`)
+          .classList.length < 2,
     };
 
     if (bottomCell.empty) {
@@ -60,23 +74,28 @@ function gravity(event) {
     event.target.classList.remove(player);
   }
 
-  checkGameOver(newID);
+  getLines(newID);
 }
 
-function checkGameOver(id) {
+function getLines(id) {
   const row = new Array(nbCol).fill("x");
   const col = new Array(nbRow).fill("x");
   const diagL = new Array(nbRow + nbCol).fill("x");
   const diagR = new Array(nbRow + nbCol).fill("x");
 
   for (let i = 0; i < nbCol; i++) {
-    if (document.getElementById(`${id[0]}${i}`).classList.length == 2) {
-      row[i] = document.getElementById(`${id[0]}${i}`).classList[1];
+    if (
+      document.getElementById(`${id.split(".")[0]}.${i}`).classList.length == 2
+    ) {
+      row[i] = document.getElementById(`${id.split(".")[0]}.${i}`).classList[1];
     }
   }
+
   for (let i = 0; i < nbRow; i++) {
-    if (document.getElementById(`${i}${id[1]}`).classList.length == 2) {
-      col[i] = document.getElementById(`${i}${id[1]}`).classList[1];
+    if (
+      document.getElementById(`${i}.${id.split(".")[1]}`).classList.length == 2
+    ) {
+      col[i] = document.getElementById(`${i}.${id.split(".")[1]}`).classList[1];
     }
   }
 
@@ -84,19 +103,19 @@ function checkGameOver(id) {
   const IndexLeft = [];
   const IndexRight = [];
 
-  while (id[0] - i >= 0 && id[1] - i >= 0) {
-    IndexLeft[0] = id[0] - i;
-    IndexLeft[1] = id[1] - i;
+  while (id[0] - i >= 0 && id.split(".")[1] - i >= 0) {
+    IndexLeft[0] = id.split(".")[0] - i;
+    IndexLeft[1] = id.split(".")[1] - i;
     i++;
   }
   i = 0;
   while (IndexLeft[0] < nbRow && IndexLeft[1] < nbCol) {
     if (
-      document.getElementById(`${IndexLeft[0]}${IndexLeft[1]}`).classList
+      document.getElementById(`${IndexLeft[0]}.${IndexLeft[1]}`).classList
         .length == 2
     ) {
       diagR[i] = document.getElementById(
-        `${IndexLeft[0]}${IndexLeft[1]}`
+        `${IndexLeft[0]}.${IndexLeft[1]}`
       ).classList[1];
     }
     IndexLeft[0]++;
@@ -104,44 +123,38 @@ function checkGameOver(id) {
     i++;
   }
   i = 0;
-  while (id[0] - i >= 0 && id[1] + i >= 0) {
-    IndexRight[0] = id[0] - i;
-    IndexRight[1] = parseInt(id[1]) + i;
+  while (id.split(".")[0] - i >= 0 && id.split(".")[1] + i >= 0) {
+    IndexRight[0] = id.split(".")[0] - i;
+    IndexRight[1] = parseInt(id.split(".")[1]) + i;
     i++;
   }
+  console.log(IndexRight);
 
   i = 0;
   while (IndexRight[0] < nbRow && IndexRight[1] >= 0 && IndexRight[1] < nbCol) {
-    console.log(IndexRight);
     if (
-      document.getElementById(`${IndexRight[0]}${IndexRight[1]}`).classList
+      document.getElementById(`${IndexRight[0]}.${IndexRight[1]}`).classList
         .length == 2
     ) {
       diagL[i] = document.getElementById(
-        `${IndexRight[0]}${IndexRight[1]}`
+        `${IndexRight[0]}.${IndexRight[1]}`
       ).classList[1];
     }
     IndexRight[0]++;
     IndexRight[1]--;
     i++;
   }
-  console.log(diagR);
+  checkGamerOver(row, col, diagL, diagR);
+}
 
-  if (
-    diagR.join("").includes("J1".repeat(4)) |
-    diagL.join("").includes("J1".repeat(4)) |
-    row.join("").includes("J1".repeat(4)) |
-    col.join("").includes("J1".repeat(4))
-  ) {
-    endGame(1);
-  } else if (
-    diagR.join("").includes("J2".repeat(4)) |
-    diagL.join("").includes("J2".repeat(4)) |
-    row.join("").includes("J2".repeat(4)) |
-    col.join("").includes("J2".repeat(4))
-  ) {
-    endGame(2);
-  }
+function checkGamerOver(...lines) {
+  lines.forEach(function (line) {
+    if (line.join("").includes("J1".repeat(pow))) {
+      endGame(1);
+    } else if (line.join("").includes("J2".repeat(pow))) {
+      endGame(2);
+    }
+  });
 }
 
 function endGame(winner) {
@@ -150,8 +163,7 @@ function endGame(winner) {
 }
 
 function launcher() {
-  generateHTMLGrid(nbCol, nbRow);
-  cellsEvent();
+  settings();
 }
 
 launcher();
